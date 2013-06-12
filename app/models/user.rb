d977501b7d9869da120b45274a12259d81db6bac
Aaron Dufall <aald212@gmail.com>
2013-06-12 11:15:13 -0500

    User helpers and improved validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
   validates :name, presence: true
   validates :email, uniqueness: true, presence: true
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :password, presence: true
   validates_confirmation_of :password
   
