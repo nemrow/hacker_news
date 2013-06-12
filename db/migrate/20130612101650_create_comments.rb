09da600e44d4385171adab01c1d3fb935fb7bed9
Jordan Nemrow <nemrow@Jordans-MacBook-Pro.local>
2013-06-12 11:01:42 -0500

    routes migrations and models done

---
+++ db/migrate/20130612101650_create_comments.rb
@@ -0,0 +1,9 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+  	create_table :comments do |c|
+  		c.string :text
+  		c.integer :user_id
+  		c.integer :post_id
+  	end
+  end
+end
