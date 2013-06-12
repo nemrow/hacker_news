09dc9c17397bb7ad3537659ab86c40a8db322cec
Aaron Dufall <aald212@gmail.com>
2013-06-12 10:56:21 -0500

    basic user auth

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,46 @@
+enable :sessions
+
+get '/' do
+  if session[:user]
+    #place and logged in behaviour here
+  end
+  erb :index
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/signup' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:user] = @user.id
+    redirect '/'
+  else
+    @errors = @user.errors
+    puts @user.errors.keys
+    erb :signup
+  end
+end
+
+get '/login' do
+  erb :login
+end
+
+
+post '/login' do
+  if User.authenticate(params[:user][:name], params[:user][:password])
+    @user = User.find_by_name(params[:user][:name])
+    session[:user] = @user.id
+    redirect '/'
+  else
+    @errors = {error: "Invalid name or password."}
+    erb :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
+end
+
---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  attr_accessor :password_confirmation
+  before_save :encrypt_password
+  validates :name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
+  validates_confirmation_of :password
+  
+
+  def self.authenticate(name, password)
+    return false if name.empty? || password.empty?
+    return false unless user = User.find_by_name(name)
+    BCrypt::Password.new(user.password) == password 
+  end
+      
+
+  private
+
+  def encrypt_password
+    self.password = BCrypt::Password.create(self.password)
+  end
+
+end
