616006a0134a3c58ac6689f5d041ca2fc76bc714
Aaron Dufall <aald212@gmail.com>
2013-06-12 17:35:09 -0500

    Fixed Post.by_votes bugs that showed a copy of every vote

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,10 @@ class Post < ActiveRecord::Base
 	has_many :votes
 
 	def self.by_votes
-		select('posts.*, coalesce(value, 0) as votes').
-    joins('left join votes on post_id=posts.id').
-    order('votes desc')
+		 joins('left join votes on votes.post_id = posts.id').
+		 select('posts.*, sum(votes.value) as vote_total').
+		 group('posts.id').
+		 order('vote_total DESC')
 	end
 
 	def add_or_update_vote(value, current_user)
